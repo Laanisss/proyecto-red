d0026510ffb6479dae1cc4582a9c53c0e9435a10
Laani <laaniiss1@gmail.com>
2026-07-30 11:03:47 -0600

    Update GeneradorReporte.java

--- src/main/java/com/uth/proyectored/server/GeneradorReporte.java
+++ src/main/java/com/uth/proyectored/server/GeneradorReporte.java
@@ -1,7 +1,9 @@
 package com.uth.proyectored.server;
 
 import com.uth.proyectored.product.Producto;
+import com.uth.proyectored.product.Venta;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JREmptyDataSource;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import java.io.ByteArrayOutputStream;
@@ -9,6 +11,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 public class GeneradorReporte {
 
@@ -32,6 +35,32 @@ public class GeneradorReporte {
             return salida.toByteArray();
         } catch (Exception e) {
             throw new JRException("Error exportando el reporte a PDF", e);
+        }
+    }
+
+    public byte[] generarFactura(Venta venta) throws JRException {
+        InputStream jrxmlStream = getClass().getClassLoader().getResourceAsStream("factura.jrxml");
+        if (jrxmlStream == null) {
+            throw new JRException("No se encontro factura.jrxml en resources");
+        }
+
+        JasperReport reporteCompilado = JasperCompileManager.compileReport(jrxmlStream);
+
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("ventaId", venta.getId());
+        parametros.put("fecha", venta.getFecha());
+        parametros.put("producto", venta.getProductoNombre());
+        parametros.put("cantidad", venta.getCantidad());
+        parametros.put("precioUnitario", venta.getPrecioUnitario());
+        parametros.put("total", venta.getTotal());
+
+        JasperPrint impresion = JasperFillManager.fillReport(reporteCompilado, parametros, new JREmptyDataSource());
+
+        try (ByteArrayOutputStream salida = new ByteArrayOutputStream()) {
+            JasperExportManager.exportReportToPdfStream(impresion, salida);
+            return salida.toByteArray();
+        } catch (Exception e) {
+            throw new JRException("Error exportando la factura a PDF", e);
         }
     }
 }
